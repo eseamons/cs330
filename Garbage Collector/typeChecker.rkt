f01923ce77971c6677d6aa3414475494e2670aae
Eric <ericseamons90@gmail.com>
2016-03-16 16:34:20 -0600

    started writing type-of function. It will give the type of a num, a bool, and a bin-num-op

--- Garbage Collector/typeChecker.rkt
+++ Garbage Collector/typeChecker.rkt
@@ -1,5 +1,7 @@
 #lang plai
 
+; type definitions
+
 (define-type Expr
   [num (n number?)]
   [id (v symbol?)]
@@ -24,12 +26,18 @@
   [t-nlist]
   [t-fun (arg Type?) (result Type?)])
 
-
+; table of binary number operations
 (define op-table
   (list
    (list '+ +)
    (list '- -)
    (list '* *)))
+
+; list of key words that can't be used as identifier
+
+(define key-word-table
+  (list 'true 'false 'iszero 'bif 'with 'fun
+        'nempty 'ncons 'empty? 'nfirst 'nrest 'app))
 
 
 ; lookup-op : (op) -> (or/c procedure? false/c)
@@ -39,57 +47,68 @@
       #f
   (second (assoc op op-table))))
 
+(define (lookup-keyword keyword)
+  (ormap (lambda (x) (eq? keyword x)) key-word-table))
+
 ; valid id
-(define (is-valid-identifier? x)
-  (and (symbol? x)
-       (eq? #f (lookup-op x))
+(define (is-valid-identifier? id)
+  (and (symbol? id)
+       (eq? #f (lookup-keyword id))
        ))
 
-; valid bin-num-op
+; valid bin-num-op expression
 (define (is-valid-bin-num-op? x)
   (and (= 3 (length x)) (procedure? (lookup-op (first x)))))
 
+; valid iszero expression
 (define (is-valid-iszero? sexp)
   (and (= 2 (length sexp)) (eq? 'iszero (first sexp))))
 
+; valid bool expression
 (define (is-boolean? bool)
   (or (eq? 'true bool) (eq? 'false bool)))
 
+; gets the boolean
 (define (get-bool bool)
   (cond
     [(eq? 'true bool) true]
     [(eq? 'false bool) false]))
 
+; valid bif expression
 (define (is-valid-bif? sexp)
   (and
    (= 4 (length sexp))
    (eq? 'bif (first sexp))))
 
+; valid ncons expression
 (define (is-valid-ncons? sexp)
   (and
    (= 3 (length sexp))
    (eq? 'ncons (first sexp))))
 
+; valid nempty expression
 (define (is-valid-nempty? sexp)
   (eq? 'nempty sexp))
 
+; valid isnempty expresion
 (define (is-valid-isnempty sexp)
   (and
    (= 2 (length sexp))
    (eq? 'nempty? (first sexp))))
 
+; valid nfirst expression
 (define (is-valid-nfirst? sexp)
   (and
    (= 2 (length sexp))
    (eq? 'nfirst (first sexp))))
-
+; valid nrest expression
 (define (is-valid-nrest? sexp)
   (and
    (= 2 (length sexp))
    (eq? 'nrest (first sexp))))
 
 
-; valid with
+; valid with expression
 (define (is-valid-with? sexp)
   (and
    (= 3 (length sexp)) ; checks if with statement has 3 items: with, list of bindings, and bodys
@@ -98,6 +117,14 @@
     (= 2 (length (second sexp)))
     (is-valid-identifier? (first (second sexp)))) ; checks if the bindings is a list and if it is not empty
    ))
+
+; valid app expression
+(define (is-valid-app? sexp)
+  (and
+   (list? sexp)
+   (not (empty? sexp))
+   (= 3 (length sexp))
+   (eq? 'app (first sexp))))
 
  
 ; parse : s-expression -> Expr
@@ -117,6 +144,9 @@
        [(is-valid-isnempty sexp) (isnempty (parse (second sexp)))]
        [(is-valid-nfirst? sexp) (nfirst (parse (second sexp)))]
        [(is-valid-nrest? sexp) (nrest (parse (second sexp)))]
+       [(is-valid-app? sexp)
+        (app (parse (second sexp))
+             (parse (third sexp)))]
        [else (error 'parse "Illegal syntax")]
       )]
     [(number? sexp) (num sexp)]
@@ -128,31 +158,74 @@
  
 ; type-of : Expr -> Type
 (define (type-of e)
-  (error 'type-of "not implemented"))
+  (type-case Expr e
+    [num (n) (t-num)]
+    [bin-num-op (op lhs rhs)
+                (if (and
+                     (t-num? (type-of lhs))
+                     (t-num? (type-of rhs)))
+                    (t-num)
+                    (error 'type-of "error"))]
+    [bool (b) (t-bool)]
+    [else (error 'type-of "not implemented")]))
+
+
+
+; parse tests
 
 ; parse num
-(parse 6)
+(test (parse 6) (num 6))
 ; parse id
-(parse 'x)
-(parse 'y)
+(test (parse 'x) (id 'x))
+(test (parse 'y) (id 'y))
 ; parse expression
-(parse '(+ 1 2))
-(parse '(* 3 4))
-(parse '(- 17 5))
+(test (parse '(+ 1 2)) (bin-num-op + (num 1) (num 2)))
+(test (parse '(* 3 4)) (bin-num-op * (num 3) (num 4)))
+(test (parse '(- 17 5)) (bin-num-op - (num 17) (num 5)))
 ; parse boolean
-(parse 'true)
-(parse 'false)
+(test (parse 'true) (bool #t))
+(test (parse 'false) (bool #f))
 ; parse bif
-(parse '(bif true 1 2))
+(test (parse '(bif true 1 2)) (bif (bool #t) (num 1) (num 2)))
 ; parse iszero
-(parse '(iszero 0))
-(parse '(ncons 6 6))
+(test (parse '(iszero 0)) (iszero (num 0)))
+; parse ncons
+(test (parse '(ncons 6 6)) (ncons (num 6) (num 6)))
+(test (parse '(ncons (nfirst 6) (nrest nempty)))
+      (ncons (nfirst (num 6)) (nrest (nempty))))
+(test (parse '(ncons (nfirst 6)
+                     (nrest
+                      (ncons (nfirst 3)
+                            (nrest nempty)))))
+      (ncons
+       (nfirst (num 6))
+       (nrest
+        (ncons
+         (nfirst (num 3))
+         (nrest (nempty))))))
+
 ; parse nempty
-(parse 'nempty)
+(test (parse 'nempty) (nempty))
 ; parse nfirst
-(parse '(nfirst 6))
-(parse '(nrest nempty))
+(test (parse '(nfirst 6)) (nfirst (num 6)))
+(test (parse '(nfirst nempty)) (nfirst (nempty)))
+(test (parse '(nrest nempty)) (nrest (nempty)))
 ; parse nempty?
-(parse '(nempty? nempty))
+(test (parse '(nempty? nempty)) (isnempty (nempty)))
 ; parse with
-(parse '(with (x 6) (+ x x)))
+(test (parse '(with (x 6) (+ x x)))
+      (with 'x (num 6) (bin-num-op + (id 'x) (id 'x))))
+; parse app
+(test (parse '(app 6 7)) (app (num 6) (num 7)))
+
+
+
+
+; type-of tests
+(test (type-of (parse 1)) (t-num))
+(test (type-of (parse 'true)) (t-bool))
+(test (type-of (parse 'false)) (t-bool))
+(test (type-of (parse '(+ 1 2))) (t-num))
+(test (type-of (parse '(* 3 4))) (t-num))
+(test (type-of (parse '(- 20 15))) (t-num))
+(test (type-of (parse '(+ 1 (* 3 (- 5 7))))) (t-num))
